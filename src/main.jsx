e055a06397eb5156e1a57a864c1605d401d5fd2d
technovahub2025 <technovahubcareer@gmail.com>
2026-08-14 11:41:30 +0530

    add basename to BrowserRouter and create vercel configuration for rewrites

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ErrorBoundary>
-      <BrowserRouter>
+      <BrowserRouter basename="/test_certificates">
         <App />
       </BrowserRouter>
     </ErrorBoundary>
